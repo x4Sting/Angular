263bd53412a256e13de1c211c2aff29893d0b08f
x4Sting <polaskalg@gmail.com>
2022-09-09 21:07:05 +0300

    First lesson

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,19 @@
 import { Component } from '@angular/core';
 
+class Item {
+  id: number;
+  purchase: string;
+  done: boolean;
+  price: number;
+
+  constructor( purchase: string, price: number, id: number) {
+    this.id = id;
+    this.purchase = purchase;
+    this.done = false;
+    this.price = price;
+  }
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +21,14 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'example';
+
+  constructor() {
+  }
+
+  items: Item[] = [
+    { id:1, purchase: 'Хлеб', done: false, price: 15},
+    { id:2, purchase: 'Батон', done: false, price: 12},
+    { id:3, purchase: 'Масло', done: true, price: 27},
+    { id:4, purchase: 'Икра', done: false, price: 60},
+  ]
 }
